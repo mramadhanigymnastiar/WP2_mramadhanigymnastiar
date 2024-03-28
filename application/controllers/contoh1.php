2ffa9c4bbc01f056a8da4cfe1fe3469789a33107
mramadhanigymnastiar <mramadhanigymnastiar@gmail.com>
2024-03-28 20:00:08 +0700

    first commit

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalan<h1>";
+        echo"Nama saya Muhammad Ramadhani Gymnastiar
+            Saya tinggal di daerah Bekasi
+            olah raga yang saya sukai adalah
+            bersepeda";
+    }
+}
---
+++ application/views/view-latihan.php
@@ -0,0 +1,14 @@
+<html>
+
+<head>
+    <title>Latihan 1</title>
+</head>
+<body>
+    Halo Kawan.. Yuk kita belajar web programing..!!!<br>
+    nilai 1 = <?= $nilai1; ?>
+    nilai 2 = <?= $nilai2; ?>
+    ini hasil dari pemodelan dengan methode penjumlahan yaitu <?=
+    $nilai1 . " + " . $nilai2 . " = " . $hasil; ?>
+</body>
+
+</html>
